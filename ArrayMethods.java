bc430d38e37e750e38e96fe80885485c3b647895
Justin Chen <jchen06@stuy.edu>
2018-11-06 15:49:01 -0500

    Should be finished

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -22,7 +22,6 @@ public class ArrayMethods {
     return sum;
   }
 
-
   public static int[] allRowSums(int[][] ary) {
     int[] sum = new int[ary.length];
     for (int y = 0; y < ary.length; y++) {
@@ -31,6 +30,7 @@ public class ArrayMethods {
     return sum;
     }
 
+  //helper method finding longest array
   public static int longestAry(int[][] ary) {
     int temp = 0;
     int longest = 0;
@@ -56,4 +56,33 @@ public class ArrayMethods {
     return sum;
   }
 
+  public static boolean isRowMagic(int[][] ary) {
+    int[] sumRow = allRowSums(ary);
+    for (int x = 0; x < ary.length-1; x++){
+      if (sumRow[x] != sumRow[x+1]) {
+        return false;
+      }
+    }
+    return true;
   }
+
+  public static boolean isColumnMagic(int[][] ary){
+    int[] sumCol = allColSums(ary);
+    for (int x = 0; x < ary.length-1; x++){
+      if (sumCol[x] != sumCol[x+1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //helper method in helping make printing easier in driver
+  public static void printArray(int[][]ary) {
+    for (int a = 0; a < ary.length; a++) {
+      for (int b = 0; b < ary[a].length; b++) {
+        System.out.print("[" +ary[a][b] + "]");
+      }
+      System.out.println("");
+    }
+  }
+}
